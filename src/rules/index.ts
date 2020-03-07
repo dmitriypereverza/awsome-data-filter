3a51a43377836541ba7449efe356c2f0c932d1ba
Дмитрий Переверза <d.pereverza@worksolutions.ru>
2020-03-07 17:50:46 +0300

    подготовка для публикации

---
+++ src/rules/index.ts
@@ -0,0 +1,23 @@
+import { betweenDates } from "./betweenDates";
+import { equalOneOf } from "./equalOneOf";
+import { equalProp } from "./equalProp";
+import { isEmptyArray } from "./isEmptyArray";
+import { lessThen } from "./lessThen";
+import { matchText } from "./matchText";
+import { moreThen } from "./moreThen";
+import { not } from "./not";
+import { someInArray } from "./someInArray";
+import { targetNumberBetweenFilterNumbers } from "./targetNumberBetweenFilterNumbers";
+
+export {
+  betweenDates,
+  equalOneOf,
+  equalProp,
+  isEmptyArray,
+  lessThen,
+  matchText,
+  moreThen,
+  not,
+  someInArray,
+  targetNumberBetweenFilterNumbers,
+};
